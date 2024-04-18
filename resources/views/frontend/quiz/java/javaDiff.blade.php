9b2ea19d65159874be2758d5797332dfdcda693f
David Borromeo <davidmatthewarsenal11@gmail.com>
2024-04-18 18:13:17 +0800

    dasdasdas

--- resources/views/frontend/quiz/java/javaDiff.blade.php
+++ resources/views/frontend/quiz/java/javaDiff.blade.php
@@ -4,11 +4,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Select Difficulty</title>
+  <title>Java Difficulty | Quiz</title>
   <link rel="stylesheet" href="/assets/css/quizDiff.css">
   <link rel="stylesheet" href="/assets/css/header.css">
   <link rel="shortcut icon" type="x-icon" href="/assets/images/Logo.svg">
-
   <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600;800&display=swap" rel="stylesheet">
   <script src="/assets/js/headermenu.js" defer></script>
--- resources/views/frontend/quiz/selectLanguage.blade.php
+++ resources/views/frontend/quiz/selectLanguage.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Quiz Select Language</title>
+  <title>Select Language | Quiz</title>
 
 <link rel="stylesheet" href="assets/css/moduleLang.css">
 <link rel="stylesheet" href="assets/css/header.css">
